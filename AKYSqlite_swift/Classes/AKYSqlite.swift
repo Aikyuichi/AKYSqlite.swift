db133616a2137a4516ef127fbf41cb4912c8f238
Aikyuichi <aikyu.sama@gmail.com>
2024-03-07 21:07:09 -0500

    0.6.0

--- AKYSqlite_swift/Classes/AKYSqlite.swift
+++ AKYSqlite_swift/Classes/AKYSqlite.swift
@@ -14,34 +14,32 @@ public class AKYSqlite {
     
     private init() {}
     
-    static public func registerDatabasePath(path: String, forKey key: String) {
+    static public func registerDatabase(path: String, forKey key: String) {
         var dbPaths = UserDefaults.standard.dictionary(forKey: AKYSQLITE_DB_PATHS) ?? [:]
         dbPaths[key] = path
         UserDefaults.standard.setValue(dbPaths, forKey: AKYSQLITE_DB_PATHS)
     }
     
-    static public func registerDatabase(name: String, fromMainBundleForKey key: String) {
+    static public func registerDatabase(name: String, fromMainBundleForKey key: String, copyToDocumentDirectory copy: Bool = false) {
         let url = NSURL(fileURLWithPath: name)
         let dbPath = Bundle.main.path(forResource: url.deletingPathExtension?.lastPathComponent, ofType: url.pathExtension)!
-        self.registerDatabasePath(path: dbPath, forKey: key)
+        if copy {
+            if let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+                let dbPathTo = (documentPath as NSString).appendingPathComponent(name)
+                if !FileManager.default.fileExists(atPath: dbPathTo) {
+                    try! FileManager.default.copyItem(atPath: dbPath, toPath: dbPathTo)
+                }
+                self.registerDatabase(path: dbPathTo, forKey: key)
+            }
+        } else {
+            self.registerDatabase(path: dbPath, forKey: key)
+        }
     }
     
     static public func registerDatabase(name: String, fromDocumentDirectoryForKey key: String) {
         if let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
             let dbPath = (documentPath as NSString).appendingPathComponent(name)
-            self.registerDatabasePath(path: dbPath, forKey: key)
-        }
-    }
-    
-    static public func registerDatabase(name: String, copyFromMainBundleToDocumentDirectoryForKey key: String) {
-        let url = NSURL(fileURLWithPath: name)
-        let dbPathFrom = Bundle.main.path(forResource: url.deletingPathExtension?.lastPathComponent, ofType: url.pathExtension)!
-        if let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
-            let dbPathTo = (documentPath as NSString).appendingPathComponent(name)
-            if !FileManager.default.fileExists(atPath: dbPathTo) {
-                try! FileManager.default.copyItem(atPath: dbPathFrom, toPath: dbPathTo)
-            }
-            self.registerDatabasePath(path: dbPathTo, forKey: key)
+            self.registerDatabase(path: dbPath, forKey: key)
         }
     }
     
@@ -56,39 +54,42 @@ public class AKYSqlite {
         return dbPaths?[key] as? String;
     }
     
-    static public func runUpdater(forKey key: String) {
-        let updates = self.getUpdates(forKey: key)
-        if updates.isEmpty {
-            return
-        }
-        for update in updates {
-            if !self.executeUpdate(update) {
-                if update.errorLevel == AKYUpdaterErrorLevel.skip {
-                    print("update failed but skipped: \(update)")
-                } else {
-                    print("update failed: \(update)")
-                    break
+    static public func runUpdater(path: String? = nil) {
+        if let path = path ?? Bundle.main.path(forResource: "updater", ofType: "json") {
+            let updates = self.getUpdates(filename: path)
+            if updates.isEmpty {
+                return
+            }
+            for update in updates {
+                if !self.executeUpdate(update) {
+                    if update.errorLevel == AKYUpdaterErrorLevel.skip {
+                        print("update failed but skipped: \(update)")
+                    } else {
+                        print("update failed: \(update)")
+                        break
+                    }
                 }
             }
+        } else {
+            print("updater.json not found in main bundle")
         }
     }
     
-    static private func getUpdates(forKey key: String) -> [UpdateItem] {
+    static private func getUpdates(filename: String) -> [UpdateItem] {
         var items: [UpdateItem] = []
-        let db = AKYDatabase(forKey: key)
-        if db.open() {
-            let stmt = db.prepareStatement(query: "SELECT id, db_key, db_version, sql, error_level FROM updater")
-            while stmt.step() {
-                items.append(UpdateItem(
-                    id: stmt.getInt(forName: "id")!,
-                    dbKey: stmt.getString(forName: "db_key")!,
-                    dbVersion: stmt.getInt(forName: "db_version")!,
-                    sqlCommands: stmt.getString(forName: "sql")!,
-                    errorLevel: AKYUpdaterErrorLevel(rawValue: stmt.getInt(forName: "error_level")!) ?? .log
-                ))
+        do {
+            if let data = try String(contentsOfFile: filename).data(using: .utf8),
+               let json = try JSONSerialization.jsonObject(with: data) as? [[String: Any]] {
+                for item in json {
+                    if let updateItem = UpdateItem(json: item) {
+                        items.append(updateItem)
+                    } else {
+                        break
+                    }
+                }
             }
-            stmt.finalize()
-            db.close()
+        } catch {
+            print(error)
         }
         return items
     }
@@ -129,19 +130,29 @@ private struct UpdateItem {
     let id: Int
     let dbKey: String
     let dbVersion: Int
-    let sqlCommands: [String]
     let errorLevel: AKYUpdaterErrorLevel
+    let sqlCommands: [String]
     
-    init(id: Int, dbKey: String, dbVersion: Int, sqlCommands: String, errorLevel: AKYUpdaterErrorLevel) {
+    init?(json: [String: Any]) {
+        guard let id = json["id"] as? Int,
+              let dbKey = json["dbKey"] as? String,
+              let dbVersion = json["dbVersion"] as? Int,
+              let errorLevelRawValue = json["errorLevel"] as? Int,
+              let errorLevel = AKYUpdaterErrorLevel(rawValue: errorLevelRawValue),
+              let sqlCommands = json["sqlCommands"] as? [String]
+        else {
+            print("updater: invalid format")
+            return nil
+        }
         self.id = id
         self.dbKey = dbKey
         self.dbVersion = dbVersion
-        self.sqlCommands = sqlCommands.split(separator: ";", omittingEmptySubsequences: true).map { String($0) }
         self.errorLevel = errorLevel
+        self.sqlCommands = sqlCommands
     }
 }
 
 private enum AKYUpdaterErrorLevel: Int {
     case skip = 0
-    case log = 1
+    case fail = 1
 }
